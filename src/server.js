7c0ed954d46ab0725d49f0ca431bcfbe819cb2cb
seroga <sergijvchichur@gmail.com>
2026-09-03 21:53:49 +0300

    fixed package.json

--- src/server.js
+++ src/server.js
@@ -25,21 +25,21 @@ app.get("/notes/:noteId", (req, res) => {
 
 app.get("/test-error", (req, res) => {
   throw new Error("Simulated server error");
-})
+});
 
 app.use((req, res, next) => {
   res.status(404).json({
     "message": "Route not found"
-  })
-})
+  });
+});
 
 app.use((err, req, res, next) => {
   const isProd = process.env.NODE_ENV === "production";
 
   res.status(500).json({
     message: isProd ? "There is some error. Please try again later" : err.message
-  })
-})
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
