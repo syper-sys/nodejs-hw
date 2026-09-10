4fbd33b0fb8702a4b1133f39258eb6e56721beca
seroga <sergijvchichur@gmail.com>
2026-09-10 14:00:03 +0300

    fix bugs

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -4,11 +4,6 @@ import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
-
-  if (!notes) {
-    throw createHttpError(404, 'Notes not found');
-  }
-
   res.status(200).json(notes);
 };
 
