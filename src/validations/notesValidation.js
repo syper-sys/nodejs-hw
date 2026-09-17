d7d6d550d6c2d85735d9c7d7143654c85588813a
seroga <sergijvchichur@gmail.com>
2026-09-17 16:56:01 +0300

    init

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -18,7 +18,7 @@ export const getAllNotesSchema = {
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().trim().allow(''),
-    sortBy: Joi.string().valid('_id', 'tag', 'search'),
+    sortBy: Joi.string().valid('_id', 'tag'),
     sortOrder: Joi.string().valid('asc', 'desc'),
   }),
 };
